3560f02543c907b7d0c8878f13f30a927ee2cdf4
Imre Mehesz <imehesz@gmail.com>
2015-06-05 13:18:07 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require("gulp");
+var babelify = require('babelify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var sass = require("gulp-sass");
+var sourceMaps = require("gulp-sourcemaps");
+var serverFactory = require('spa-server');
+
+gulp.task('webserver', function () {
+  var server = serverFactory.create({
+    path: './target',
+    port: 8000,
+    fallback: {}
+  });
+
+  server.start();
+});
+
+gulp.task("sass", function(){
+    gulp.src("app/css/style.scss")
+    .pipe(sourceMaps.init())
+    .pipe(sass())
+    .pipe(sourceMaps.write())
+    .pipe(gulp.dest("target/"));
+});
+
+gulp.task("modules", function(){
+    browserify({
+      entries: "app/app.es6.js",
+      debug: true
+    }).
+    transform(babelify).
+    bundle().
+    pipe(source("app.js")).
+    pipe(gulp.dest("target/"));
+});
+
+gulp.task("copyIndex", function(){
+    gulp.src(["app/index.html"])
+    .pipe(gulp.dest("target/"));
+});
+
+gulp.task("copyLibs", function(){
+  gulp.src(["app/libs/**/*"])
+  .pipe(gulp.dest("target/libs"));
+});
+
+gulp.task("watchJs", function(){
+    gulp.watch(["app/app.es6.js", "app/modules/**/*.js", "app/css/**/*.scss", "app/index.html"], ["build"]);
+});
+
+gulp.task("build", ["modules","sass", "copyIndex", "copyLibs"]);
+gulp.task("default", ["build","watchJs","webserver"]);
