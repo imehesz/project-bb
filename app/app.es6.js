7a8a9355c0cfaa2a979cf611a5c0e1e08e68fb2d
Imre Mehesz <imehesz@gmail.com>
2015-06-09 12:56:10 -0400

    added Angular and started logic

--- app/app.es6.js
+++ app/app.es6.js
@@ -1,16 +1,52 @@
 import book from "./modules/BookModule";
 
-var asvBook = new book({language:"asv"});
-var hunBook = new book({
-  language:"hun",
-  loadCallback: function() {
-    console.log("mooo");
-    console.log(this.getVersesInChapter(1,1));
+class App {
+  constructor(opts) {
+    opts = opts || {};
+    this.bookOneId = opts.bookOne;
+    this.bookTwoId = opts.bookTwo;
   }
-});
 
-class Test {
-  constructor() {
-    console.log("TESTING!!");
+  ngController ($scope) {
+    $scope.moo = "baa";
+    $scope.selectedBookId = 0;
+    $scope.selectedChapterId = 0;
+
+    new book({
+      language: app.bookOneId || "asv", 
+      loadCallback: function() {
+        $scope.bookOne = this;
+      }
+    });
+
+    new book({
+      language: app.bookTwoId || "asv", 
+      loadCallback: function() {
+        $scope.bookTwo = this;
+      }
+    });
+
+    $scope.setBookId = function(id) {
+      $scope.selectedBookId = id || 0;
+      if ($scope.selectedBookId) {
+        $scope.chapterIds = $scope.bookOne.getChapterIdsInBook(id);
+      }
+    }
+
+    $scope.setChapterId = function(id) {
+      $scope.selectedChapterId = id || 0;
+      if ($scope.selectedChapterId) {
+        $scope.bookOneVerses = $scope.bookOne.getVersesInChapter($scope.selectedBookId, $scope.selectedChapterId);
+        $scope.bookTwoVerses = $scope.bookTwo.getVersesInChapter($scope.selectedBookId, $scope.selectedChapterId);
+      }
+    }
   }
 }
+
+var app = new App({
+  bookOne: "asv",
+  bookTwo: "hun"
+});
+
+var webApp = angular.module("webApp", []);
+webApp.controller("AppController", app.ngController);
