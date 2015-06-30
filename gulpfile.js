51105b1d33714709e9242b757e0647bba49ebfca
Imre Mehesz <imehesz@gmail.com>
2015-06-30 12:01:19 -0400

    Added  feature

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,9 @@ var source = require('vinyl-source-stream');
 var sass = require("gulp-sass");
 var sourceMaps = require("gulp-sourcemaps");
 var serverFactory = require('spa-server');
+var shell = require("gulp-shell");
 
+// DEV
 gulp.task('webserver', function () {
   var server = serverFactory.create({
     path: './target',
@@ -54,5 +56,39 @@ gulp.task("watchJs", function(){
   gulp.watch(["app/app.es6.js", "app/modules/**/*.js", "app/css/**/*.scss", "app/index.html"], ["build"]);
 });
 
-gulp.task("build", ["modules","sass", "copyIndex", "copyLibs", "copyData"]);
+// PROD
+gulp.task("depIndex", function(){
+    gulp.src(["app/index.html"])
+    .pipe(gulp.dest("deploy/"));
+});
+
+gulp.task("depCopyLibs", function(){
+  gulp.src(["app/libs/**/*"])
+  .pipe(gulp.dest("deploy/libs"));
+});
+
+gulp.task("depModules", function(){
+    browserify({
+      entries: "app/app.es6.js",
+      debug: false
+    }).
+    transform(babelify).
+    bundle().
+    pipe(source("app.js")).
+    pipe(gulp.dest("deploy/"));
+});
+
+gulp.task("depSass", function(){
+    gulp.src("app/css/style.scss")
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest("deploy/css/"));
+});
+
+gulp.task("depRsync", shell.task([
+  "rsync -a deploy/ [SERVER/HERE/with/folder]",
+  "rm -rf deploy"
+]));
+
+gulp.task("deploy", ["depModules", "depSass", "depIndex", "depCopyLibs", "depRsync"]);
+gulp.task("build", ["modules","sass", "copyIndex", "copyLibs" ]);
 gulp.task("default", ["build","watchJs","webserver"]);
